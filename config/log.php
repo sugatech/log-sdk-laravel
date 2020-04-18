baa3112b005b4e30f5cdb028976985a85052e48c
Huỳnh Phan (Phil) <huynhphan89@gmail.com>
2020-04-18 11:51:13 +0700

    [#2] Dry run config (Review)

--- config/log.php
+++ config/log.php
@@ -4,5 +4,5 @@ return [
     'api_url' => env('LOG_API_URL'),
     'access_token' => env('LOG_ACCESS_TOKEN'),
     'delimiter' => env('LOG_DELIMITER', '||'),
-    'dry_run' => env('LOG_DRY_RUN', false)
+    'dry_run' => env('LOG_DRY_RUN', false),
 ];
