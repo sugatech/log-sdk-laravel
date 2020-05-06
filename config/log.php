7f631ea2165e00bd6d87b3cbe7b8deafdda67f2e
Huynh Phan <huynhphan89@gmail.com>
2020-05-06 11:46:16 +0700

    [#9] Use oauth2-client-credentials-laravel lib (Review)

--- config/log.php
+++ config/log.php
@@ -2,10 +2,10 @@
 
 return [
     'api_url' => env('LOG_API_URL'),
-    'dry_run' => env('LOG_DRY_RUN', false),
     'oauth' => [
         'url' => env('LOG_OAUTH_URL', env('LOG_API_URL').'/oauth/token'),
         'client_id' => env('LOG_OAUTH_CLIENT_ID'),
         'client_secret' => env('LOG_OAUTH_CLIENT_SECRET'),
     ],
+    'dry_run' => env('LOG_DRY_RUN', false),
 ];
